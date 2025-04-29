b7d728f3746c4e01467e0331486f3fdfb0fee09a
Aya22-g <moon22aikou@gmail.com>
2025-04-29 17:04:24 +0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -10,12 +10,13 @@ public class Main {
         // для решения примените лямбда-выражения, 
         
         // каждый кот должен уметь выполнять что-то своё
-        var cats = new ArrayList<ActiveCat>();
-        cats.add(new ActiveCat(???));
-        cats.add(new ActiveCat(???));
-        cats.add(new ActiveCat(???));
-        // добавьте ещё два-три кота, с совершенно другими действиями
-
+        cats.add(new ActiveCat(ActiveCat::jump));
+        cats.add(new ActiveCat(ActiveCat::sleep));
+        cats.add(new ActiveCat(ActiveCat::eat));
+        cats.add(new ActiveCat(ActiveCat::purr));
+        cats.add(new ActiveCat(ActiveCat::play));
+        cats.add(new ActiveCat(ActiveCat::hunts)) 
+        
         cats.forEach(ActiveCat::doAction);
     }
 
